42f23b85e03219561e967d919faa318683393c57
André Rodrigues <andre.rodrigues@agencian1.com.br>
2024-04-11 17:06:21 -0400

    change Coupon

--- sections/Content/Coupon.tsx
+++ sections/Content/Coupon.tsx
@@ -1,3 +1,13 @@
-import { Coupon } from "deco-sites/deco-andre/components/coupon/index.tsx";
+export interface Props {
+  title: string;
+  description?: string;
+}
 
-export default Coupon;
+export default function Coupon({ title, description }: Props) {
+  return (
+    <section className="w-full flex justify-center flex-col">
+      <p className="text-center">Use o cupom: {title}</p>
+      {!!description && <p>{description}</p>}
+    </section>
+  );
+}
