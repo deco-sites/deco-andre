cc33ddc5c0edf71951e08876d0439443357f2110
André Rodrigues <andre.rodrigues@agencian1.com.br>
2024-04-14 16:26:22 -0400

    change PartialImageGallery styles

--- sections/Images/PartialImageGallery.tsx
+++ sections/Images/PartialImageGallery.tsx
@@ -23,14 +23,16 @@ export default function PartialImageGallery(
     <section class="w-full flex flex-col p-8 gap-8">
       <h2 class="text-2xl">{title}</h2>
 
-      <ul class="w-full grid grid-cols-3 gap-4 place-items-center">
+      <ul class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 place-items-center">
         {images.slice(0, pagination * images_by_pagination).map((image) => (
-          <Image
-            src={image}
-            width={300}
-            height={300}
-            class="w-full h-full object-contain rounded-lg"
-          />
+          <li class="w-full h-full">
+            <Image
+              src={image}
+              width={300}
+              height={300}
+              class="w-full h-full object-contain rounded-lg transition-all duration-500 hover:scale-110"
+            />
+          </li>
         ))}
       </ul>
 
