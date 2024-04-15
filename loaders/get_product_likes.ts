6f5b67fb91ac49d367b89d886e69c4abf8a3701f
André Rodrigues <andre.rodrigues@agencian1.com.br>
2024-04-15 02:24:54 -0400

    create get_product_likes loader

---
+++ loaders/get_product_likes.ts
@@ -0,0 +1,31 @@
+export interface Props {
+  product_id: number;
+}
+
+export interface Return {
+  product_likes: number;
+}
+
+export default async function get_product_likes(
+  { product_id }: Props,
+  _req: Request,
+  _ctx: unknown,
+): Promise<Return> {
+  const response = await fetch(
+    "https://camp-api.deco.cx/event/" + product_id,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": "teste123",
+        "x-api-token": "deco-andre",
+      },
+      credentials: "include",
+    },
+  );
+
+  if (!response.ok) return { product_likes: 0 };
+
+  const data = await response.json();
+
+  return { product_likes: data.product };
+}
