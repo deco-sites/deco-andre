73c2e58dfc33b9cf9e2dd583bf2392ee96bd6db0
André Rodrigues <andre.rodrigues@agencian1.com.br>
2024-04-16 04:52:38 -0400

    create HorizontalProductCardWithoutVariant section

--- flags/multivariate/ProductVariant.ts
+++ flags/multivariate/ProductVariant.ts
@@ -1,11 +1,11 @@
-import type { Product as ProductSchema } from "apps/commerce/types.ts";
+import type { Product } from "apps/commerce/types.ts";
 export { onBeforeResolveProps } from "apps/website/utils/multivariate.ts";
 import { MultivariateFlag } from "deco/blocks/flag.ts";
 import multivariate, {
   MultivariateProps,
 } from "apps/website/utils/multivariate.ts";
 
-export type ProductVariant = ProductSchema;
+export type ProductVariant = Product;
 
 /**
  * @title Product Variants
--- manifest.gen.ts
+++ manifest.gen.ts
@@ -54,18 +54,19 @@ import * as $$$$$$37 from "./sections/Miscellaneous/Slide.tsx";
 import * as $$$$$$38 from "./sections/Newsletter/Newsletter.tsx";
 import * as $$$$$$39 from "./sections/Product/HorizontalProductCard.tsx";
 import * as $$$$$$40 from "./sections/Product/HorizontalProductCardItem.tsx";
-import * as $$$$$$41 from "./sections/Product/ImageGalleryFrontBack.tsx";
-import * as $$$$$$42 from "./sections/Product/NotFound.tsx";
-import * as $$$$$$43 from "./sections/Product/NotFoundChallenge.tsx";
-import * as $$$$$$44 from "./sections/Product/ProductDetails.tsx";
-import * as $$$$$$45 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$46 from "./sections/Product/ProductShelfTabbed.tsx";
-import * as $$$$$$47 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$48 from "./sections/Product/ShelfWithImage.tsx";
-import * as $$$$$$49 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$50 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$51 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$52 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$41 from "./sections/Product/HorizontalProductCardWithoutVariant.tsx";
+import * as $$$$$$42 from "./sections/Product/ImageGalleryFrontBack.tsx";
+import * as $$$$$$43 from "./sections/Product/NotFound.tsx";
+import * as $$$$$$44 from "./sections/Product/NotFoundChallenge.tsx";
+import * as $$$$$$45 from "./sections/Product/ProductDetails.tsx";
+import * as $$$$$$46 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$47 from "./sections/Product/ProductShelfTabbed.tsx";
+import * as $$$$$$48 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$49 from "./sections/Product/ShelfWithImage.tsx";
+import * as $$$$$$50 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$51 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$52 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$53 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "loaders": {
@@ -122,19 +123,21 @@ const manifest = {
       $$$$$$39,
     "deco-sites/deco-andre/sections/Product/HorizontalProductCardItem.tsx":
       $$$$$$40,
-    "deco-sites/deco-andre/sections/Product/ImageGalleryFrontBack.tsx":
+    "deco-sites/deco-andre/sections/Product/HorizontalProductCardWithoutVariant.tsx":
       $$$$$$41,
-    "deco-sites/deco-andre/sections/Product/NotFound.tsx": $$$$$$42,
-    "deco-sites/deco-andre/sections/Product/NotFoundChallenge.tsx": $$$$$$43,
-    "deco-sites/deco-andre/sections/Product/ProductDetails.tsx": $$$$$$44,
-    "deco-sites/deco-andre/sections/Product/ProductShelf.tsx": $$$$$$45,
-    "deco-sites/deco-andre/sections/Product/ProductShelfTabbed.tsx": $$$$$$46,
-    "deco-sites/deco-andre/sections/Product/SearchResult.tsx": $$$$$$47,
-    "deco-sites/deco-andre/sections/Product/ShelfWithImage.tsx": $$$$$$48,
-    "deco-sites/deco-andre/sections/Product/Wishlist.tsx": $$$$$$49,
-    "deco-sites/deco-andre/sections/Social/InstagramPosts.tsx": $$$$$$50,
-    "deco-sites/deco-andre/sections/Social/WhatsApp.tsx": $$$$$$51,
-    "deco-sites/deco-andre/sections/Theme/Theme.tsx": $$$$$$52,
+    "deco-sites/deco-andre/sections/Product/ImageGalleryFrontBack.tsx":
+      $$$$$$42,
+    "deco-sites/deco-andre/sections/Product/NotFound.tsx": $$$$$$43,
+    "deco-sites/deco-andre/sections/Product/NotFoundChallenge.tsx": $$$$$$44,
+    "deco-sites/deco-andre/sections/Product/ProductDetails.tsx": $$$$$$45,
+    "deco-sites/deco-andre/sections/Product/ProductShelf.tsx": $$$$$$46,
+    "deco-sites/deco-andre/sections/Product/ProductShelfTabbed.tsx": $$$$$$47,
+    "deco-sites/deco-andre/sections/Product/SearchResult.tsx": $$$$$$48,
+    "deco-sites/deco-andre/sections/Product/ShelfWithImage.tsx": $$$$$$49,
+    "deco-sites/deco-andre/sections/Product/Wishlist.tsx": $$$$$$50,
+    "deco-sites/deco-andre/sections/Social/InstagramPosts.tsx": $$$$$$51,
+    "deco-sites/deco-andre/sections/Social/WhatsApp.tsx": $$$$$$52,
+    "deco-sites/deco-andre/sections/Theme/Theme.tsx": $$$$$$53,
   },
   "matchers": {
     "deco-sites/deco-andre/matchers/UTM.ts": $$$$$$$0,
